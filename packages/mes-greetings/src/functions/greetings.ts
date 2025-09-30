5b6921504ed6f21f1fc3acc38260302c93891219
Ga-Alves <gabrielalvesreis@gmail.com>
2025-09-30 16:48:55 -0300

    feat: :sparkles: add saldacao function

--- packages/mes-greetings/src/functions/greetings.ts
+++ packages/mes-greetings/src/functions/greetings.ts
@@ -3,3 +3,7 @@ import { GreetingsParamT } from "./greetings.type";
 export function greetings(param: GreetingsParamT) {
     console.log('Hello ' + param.name + " " + param.lastName);
 }
+
+export function saldacao(param: GreetingsParamT) {
+    console.log('Ola ' + param.name + " " + param.lastName);
+}
