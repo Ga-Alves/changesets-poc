cae8f6e0f787713695d74ef984c3490b03227de6
Ga-Alves <gabrielalvesreis@gmail.com>
2025-09-30 16:15:49 -0300

    feat: :sparkles: add sub function

--- packages/mes-math/src/funtions/base-functions.ts
+++ packages/mes-math/src/funtions/base-functions.ts
@@ -3,3 +3,7 @@ import { SumParamsT } from "./base-functions.type";
 export function sum(param: SumParamsT) {
     return param.a + param.b;
 }
+
+export function sub(a:number, b:number){
+    return a - b;
+}
